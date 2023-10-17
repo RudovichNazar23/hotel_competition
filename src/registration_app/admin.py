24f5e38b03324baade79e7ac0d30aa2692b76198
RudovichNazar23 <n.rudovich2005@gmail.com>
2023-10-17 13:22:46 +0200

    added models for registration app and made migrations

--- src/registration_app/admin.py
+++ src/registration_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import HighSchool, Guardian, TeamMember, SchoolTeam
 
-# Register your models here.
+admin.site.register(HighSchool)
+admin.site.register(Guardian)
+admin.site.register(TeamMember)
+admin.site.register(SchoolTeam)
