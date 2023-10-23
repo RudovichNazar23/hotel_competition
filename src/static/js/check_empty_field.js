a437a8dbd4876e052d943ee0dd3f90afa54ca9de
RudovichNazar23 <n.rudovich2005@gmail.com>
2023-10-23 20:06:48 +0200

    added new js functions which check if field is empty

---
+++ src/static/js/check_empty_field.js
@@ -0,0 +1,25 @@
+
+function checkFieldIsEmpty(event){
+    let input = event.target;
+    if(!input.value){
+        input.classList.add("invalid_field");
+        input.nextElementSibling.classList.add("p-2", "m-2", "text-danger");
+        input.nextElementSibling.innerHTML = "This field is empty";
+    }
+};
+
+function focusFormField(event){
+    let input = event.target;
+    if (input.classList.contains("invalid_field")){
+        input.classList.remove("invalid_field");
+        input.nextElementSibling.classList.remove("p-2", "m-2", "text-danger");
+        input.nextElementSibling.innerHTML = "";
+    }
+};
+
+let inputs = document.body.querySelectorAll("input");
+
+for(let input of inputs){
+    input.addEventListener("blur", checkFieldIsEmpty);
+    input.addEventListener("focus", focusFormField);
+};
