79ba0e5db227d2997f17f2636a5e20c2bd73de7d
RudovichNazar23 <n.rudovich2005@gmail.com>
2023-11-09 20:37:17 +0100

    edited sendFormDatahandler

--- src/service/mixins/get_form_data_mixin.py
+++ src/service/mixins/get_form_data_mixin.py
@@ -4,3 +4,9 @@ class GetFormDataMixin:
         for field in form_fields:
             form_data[field] = self.request.POST.get(field)
         return form_data
+    
+    def check_form_data(self, form_data: dict):
+        for key in form_data:
+            if not form_data[key]:
+                return False
+        return True
--- src/static/js/create_school_team.js
+++ src/static/js/create_school_team.js
@@ -1,35 +1,58 @@
-function createFormDataObject(){
-    let form_data = new FormData();
-    for(let input of document.body.querySelectorAll("input")){
-        if(input.type === "file"){
-            form_data.append(input.name, input.files[0]);
+class SendFormDataHandler{
+
+    handleEvent(event){
+        event.preventDefault();
+        if(this.checkErrors()){
+            return;
         }
         else{
-            form_data.append(input.name, input.value);
+            this.sendFormData();
         }
-    }
-    return form_data;
-};
+    };
 
-function sendFormData(event){
-    event.preventDefault();
-    $.ajax(
-        {
-            "type": "POST",
-            "url": document.getElementById("registration_form").action,
-            "data": createFormDataObject(),
-            headers: {"X-CSRFTOKEN": getCookie("csrftoken")},
-            processData: false,
-            contentType: false,
-            success: function(response){
-                window.location.replace(window.location.origin + "/registration/" + `${response.success_url_name}`);
-            },
+    createFormDataObject(){
+        let form_data = new FormData();
+        for(let input of document.body.querySelectorAll("input")){
+            if(input.type === "file"){
+                form_data.append(input.name, input.files[0]);
+            }
+            else{
+                form_data.append(input.name, input.value);
+            }
         }
-    );
+        return form_data;
+    };
     
+    sendFormData(){
+        $.ajax(
+            {
+                "type": "POST",
+                "url": document.getElementById("registration_form").action,
+                "data": this.createFormDataObject(),
+                headers: {"X-CSRFTOKEN": getCookie("csrftoken")},
+                processData: false,
+                contentType: false,
+                success: function(response){
+                    window.location.replace(window.location.origin + "/registration/" + `${response.success_url_name}`);
+                },
+            }
+        );
+    };
+
+    checkErrors(){
+        let inputs = document.body.querySelectorAll("input");
+        let button = document.getElementById("submit_button");
+
+        for(let input of inputs){
+            if(input.classList.contains("invalid_field")){
+                input.scrollIntoView({"behavior": "smooth", "block": "end", "inline": "nearest"});
+                return true;
+            }
+        };
+    };
 };
 
 
 const submit_button = document.getElementById("submit_button");
-submit_button.addEventListener("click", sendFormData);
+submit_button.addEventListener("click", new SendFormDataHandler());
 
