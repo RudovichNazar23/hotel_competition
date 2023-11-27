88a37d613a0f2beb9a15bb38e31e29d60e3cb28f
RudovichNazar23 <n.rudovich2005@gmail.com>
2023-11-27 22:23:12 +0100

    added new test for create school team view

--- src/registration_app/tests/test_views.py
+++ src/registration_app/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
 from ..models import HighSchool
 
 
@@ -38,8 +39,10 @@ class TestRegistrationView(TestCase):
 class TestCreateSchoolTeamView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.guardian_clause = SimpleUploadedFile("guardian.txt", b"file_content")
+        self.member1_clause = SimpleUploadedFile("member1.txt", b"file_content")
         self.url = reverse("create_school_team")
-        self.form_data1 = {
+        self.form_correct_data = {
             "full_name": "School1",
             "short_name": "schl1",
             "city": "Warszawa",
@@ -49,25 +52,29 @@ class TestCreateSchoolTeamView(TestCase):
             "school_email": "school1@gmail.com",
             "guardian_name": "Karol",
             "guardian_surname": "Siatka",
-            "guardian_mobile_phone": "+48576134323",
+            "guardian_mobile_phone": "+48576115323",
             "guardian_email": "guard1@gmail.com",
             "member_name": "Nazar",
             "member_surname": "Rudovich",
+            "member_clause": self.guardian_clause,
+            "guardian_clause": self.member1_clause,
         }
-        self.form_data2 = {
-            "full_name": "School1",
+        self.form_incorrect_data = {
+            "full_name": "School2",
             "short_name": "schl1",
             "city": "Warszawa",
             "post_code": "10-200",
             "street": "Wiejska 20",
-            "school_mobile_phone": "+1111111111111111",
-            "school_email": "jndfjkbhjbehjvbsdhj",
+            "school_mobile_phone": "dfnvjbdbjvdfnvbdfn",
+            "school_email": "school2gmail",
             "guardian_name": "Karol",
             "guardian_surname": "Siatka",
-            "guardian_mobile_phone": "bdfvbdbfvndf",
-            "guardian_email": "1128921fjsbdj",
+            "guardian_mobile_phone": "+4857",
+            "guardian_email": "guard2@gmailcom",
             "member_name": "Steve",
             "member_surname": "Jobs",
+            "member_clause": self.member1_clause,
+            "guardian_clause": self.guardian_clause
         }
 
     def test_GET_request(self):
@@ -77,14 +84,13 @@ class TestCreateSchoolTeamView(TestCase):
         print({self.__class__.__name__: "Test1", "result": "success"})
 
     def test_correct_data_saving(self):
-        response = self.client.post(path=self.url, data=self.form_data1)
+        response = self.client.post(path=self.url, data=self.form_correct_data)
         self.assertIn("status", response.json())
         self.assertEquals(response.json()["status"], 200)
 
         print({self.__class__.__name__: "Test2", "result": "success"})
 
     def test_incorrect_data_saving(self):
-        response = self.client.post(path=self.url, data=self.form_data2)
-        print(response.json())
-        self.assertEquals(response.json()["status"], 200)
-
+        response = self.client.post(path=self.url, data=self.form_incorrect_data)
+        self.assertEquals(response.json()["status"], 400)
+        print({self.__class__.__name__: "Test", "result": "success"})
