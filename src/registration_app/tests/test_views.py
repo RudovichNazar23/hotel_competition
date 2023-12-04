0ac30f8b37ae15bc670b661c619404d7f47c0ce7
RudovichNazar23 <n.rudovich2005@gmail.com>
2023-12-04 20:35:49 +0100

    added more testcases for current view and edited some setup objects

--- src/registration_app/tests/test_views.py
+++ src/registration_app/tests/test_views.py
@@ -25,15 +25,20 @@ class TestRegistrationView(TestCase):
         self.assertIn("guardian_form", response.context.keys())
         self.assertIn("team_member_form", response.context.keys())
 
-        print({self.__class__.__name__: "Test1", "result": "success"})
+        print({self.__class__.__name__: "Tests if registration view returns GET response correctly", "result": "Successfully"})
 
-    def test_registration_page_POST(self):
+    def test_registration_page_POST_checks_unique_field_already_exists(self):
         response = self.client.post(path=self.url, data={"field_name": "full_name", "field_value": self.school1_object.full_name})
-        self.assertEquals(response.status_code, 200)
         self.assertEquals(response.json()["status"], 400)
         self.assertIn("message", response.json())
 
-        print({self.__class__.__name__: "Test2", "result": "success"})
+        print({self.__class__.__name__: "Tests if registration view returns response_status=400, and checks if the sent field value already exists", "result": "Successfully"})
+
+    def test_registration_page_POST_returns_field_not_exists(self):
+        response = self.client.post(path=self.url, data={"field_name": "school_email", "field_value": "school228@gmail.com"})
+        self.assertEquals(response.json()["status"], 200)
+
+        print({self.__class__.__name__: "Tests if registration page returns response_status=200, and checks if the sent field value doesn't exist", "result": "Successfully"})
 
 
 class TestCreateSchoolTeamView(TestCase):
@@ -41,8 +46,11 @@ class TestCreateSchoolTeamView(TestCase):
         self.client = Client()
         self.guardian_clause = SimpleUploadedFile("guardian.txt", b"file_content")
         self.member1_clause = SimpleUploadedFile("member1.txt", b"file_content")
+        self.member2_clause = SimpleUploadedFile("member2.txt", b"file_content")
+
         self.url = reverse("create_school_team")
-        self.form_correct_data = {
+
+        self.form_correct_data_with_one_member = {
             "full_name": "School1",
             "short_name": "schl1",
             "city": "Warszawa",
@@ -56,9 +64,31 @@ class TestCreateSchoolTeamView(TestCase):
             "guardian_email": "guard1@gmail.com",
             "member_name": "Nazar",
             "member_surname": "Rudovich",
-            "member_clause": self.guardian_clause,
-            "guardian_clause": self.member1_clause,
+            "member_clause": self.member1_clause,
+            "guardian_clause": self.guardian_clause,
         }
+
+        self.form_correct_data_with_two_members = {
+            "full_name": "School1",
+            "short_name": "schl1",
+            "city": "Warszawa",
+            "post_code": "10-200",
+            "street": "Wiejska 20",
+            "school_mobile_phone": "+48576134323",
+            "school_email": "school1@gmail.com",
+            "guardian_name": "Karol",
+            "guardian_surname": "Siatka",
+            "guardian_mobile_phone": "+48576115323",
+            "guardian_email": "guard1@gmail.com",
+            "member_name": "Nazar",
+            "member_surname": "Rudovich",
+            "member_clause": self.member1_clause,
+            "guardian_clause": self.guardian_clause,
+            "second_member_name": "Steve",
+            "second_member_surname": "Jobs",
+            "second_member_clause": self.member2_clause
+        }
+
         self.form_incorrect_data = {
             "full_name": "School2",
             "short_name": "schl1",
@@ -74,7 +104,7 @@ class TestCreateSchoolTeamView(TestCase):
             "member_name": "Steve",
             "member_surname": "Jobs",
             "member_clause": self.member1_clause,
-            "guardian_clause": self.guardian_clause
+            "guardian_clause": self.guardian_clause,
         }
 
     def test_GET_request(self):
@@ -83,14 +113,25 @@ class TestCreateSchoolTeamView(TestCase):
 
         print({self.__class__.__name__: "Test1", "result": "success"})
 
-    def test_correct_data_saving(self):
-        response = self.client.post(path=self.url, data=self.form_correct_data)
+    def test_correct_data_saving_with_one_member(self):
+        response = self.client.post(path=self.url, data=self.form_correct_data_with_one_member)
         self.assertIn("status", response.json())
+        self.assertIn("message", response.json())
         self.assertEquals(response.json()["status"], 200)
 
-        print({self.__class__.__name__: "Test2", "result": "success"})
+        print({self.__class__.__name__: "Tests if correct form data has been saved correctly only with one member", "result": "Successfully"})
+
+    def test_correct_data_saving_with_two_members(self):
+        response = self.client.post(path=self.url, data=self.form_correct_data_with_two_members)
+
+        self.assertIn("status", response.json())
+        self.assertIn("message", response.json())
+        self.assertEquals(response.json()["status"], 200)
+
+        print({self.__class__.__name__: "Tests if correct form data has been saved correctly with two members", "result": "Successfully"})
 
     def test_incorrect_data_saving(self):
         response = self.client.post(path=self.url, data=self.form_incorrect_data)
         self.assertEquals(response.json()["status"], 400)
-        print({self.__class__.__name__: "Test", "result": "success"})
+
+        print({self.__class__.__name__: "Tests if incorrect form data wasn't saved", "result": "Successfully"})
--- src/service/get_object.py
+++ src/service/get_object.py
@@ -3,4 +3,5 @@ def get_model_object(model, **kwargs):
     try:
         return model.object.get(**kwargs)
     except Exception as ex:
+        print(ex)
         return None
