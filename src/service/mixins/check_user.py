a41a9ecd0c552e7a631a7571071c9b0152e13243
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-01-10 21:45:50 +0100

    Added new view which returns not activated teams, also added a template for that. Edited check_user mixin and urls at admin_app

--- src/service/mixins/check_user.py
+++ src/service/mixins/check_user.py
@@ -6,4 +6,4 @@ class CheckUserMixin:
         if self.request.user.is_anonymous:
             return redirect(to="registration/form")
         else:
-            return redirect(to="administrator/home")
+            return redirect(to="administrator/activated_school_teams")
