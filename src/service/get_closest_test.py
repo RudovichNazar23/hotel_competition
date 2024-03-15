a17bfb05f270ca7ab86d916f97d1a44d1c008a00
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-03-15 18:51:51 +0100

    added login for test

--- src/service/get_closest_test.py
+++ src/service/get_closest_test.py
@@ -1,5 +1,4 @@
 import datetime
-from datetime import timedelta
 from test_app.models import Test
 
 from .get_all_model_objects import get_all_model_objects
@@ -10,10 +9,5 @@ def get_closest_test_by_opening_date():
     current_date = datetime.date.today()
 
     greater = all_tests.filter(test_opening_date__gte=current_date).order_by("test_opening_date").first()
-    less = all_tests.filter(test_opening_date__lte=current_date).order_by("-test_opening_date").first()
 
-    if greater and less:
-        return greater if abs(greater.test_opening_date - current_date) < abs(
-            less.test_opening_date - current_date) else less
-    else:
-        return greater or less
+    return greater
