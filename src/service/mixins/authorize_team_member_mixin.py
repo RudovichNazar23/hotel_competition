dad988b34cdc43f6f7ddb49a59592bd3fecbd9de
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-03-19 11:59:36 +0100

    added new mixin for team member to get authorized for test

---
+++ src/service/mixins/authorize_team_member_mixin.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from ..get_model_object_by_uidb import get_model_object_by_uidb
+
+
+class AuthorizeTeamMemberMixin:
+    authorize_team_member_model = None
+
+    def dispatch(self, request, *args, **kwargs):
+        team_member = get_model_object_by_uidb(model=self.authorize_team_member_model, uidb64=kwargs["member_uidb64"])
+        if not team_member or "member_uidb64" not in request.session.keys():
+            return render(request=request, template_name="test_app/error_test_login.html", context={
+                "Error": "Brak dostępu do tej strony"
+            })
+        return super().dispatch(request, *args, **kwargs)
--- src/test_app/views.py
+++ src/test_app/views.py
@@ -12,11 +12,14 @@ from django.utils.encoding import force_bytes
 
 from service.get_closest_test import get_closest_test_by_opening_date
 from service.authenticate_team_member import authenticate_team_member
-from service.get_model_object import get_model_object
+from service.get_model_object_by_uidb import get_model_object_by_uidb
 
 from service.mixins.check_opened_test import CheckOpenedTestMixin
+from service.mixins.authorize_team_member_mixin import AuthorizeTeamMemberMixin
 
 from registration_app.models import SchoolTeam
+
+from registration_app.models import TeamMember
 
 
 class TestLoginView(CheckOpenedTestMixin, View):
@@ -26,18 +29,16 @@ class TestLoginView(CheckOpenedTestMixin, View):
     }
 
     def get(self, request, uidb64):
-        school_team_pk = force_str(urlsafe_base64_decode(uidb64))
-        school_team = get_model_object(model=SchoolTeam, pk=school_team_pk)
-        self.context["uidb64"] = uidb64
-
+        school_team = get_model_object_by_uidb(model=SchoolTeam, uidb64=uidb64)
         if not school_team:
             return render(request=request, template_name="test_app/error_test_login_page.html", context={})
+
+        self.context["uidb64"] = uidb64
         self.context["school_team"] = school_team
         return render(request=request, template_name=self.template_name, context=self.context)
 
     def post(self, request, uidb64):
-        school_team_pk = force_str(urlsafe_base64_decode(uidb64))
-        school_team = get_model_object(model=SchoolTeam, pk=school_team_pk)
+        school_team = get_model_object_by_uidb(model=SchoolTeam, uidb64=uidb64)
         test = get_closest_test_by_opening_date()
         form = TestLoginForm(request.POST)
 
@@ -48,6 +49,7 @@ class TestLoginView(CheckOpenedTestMixin, View):
             team_member = authenticate_team_member(school_team_object=school_team, name=member_name, surname=member_surname)
             if team_member:
                 pk = urlsafe_base64_encode(force_bytes(team_member.pk))
+                request.session["member_uidb64"] = pk
                 return redirect(reverse("test_detail", kwargs={"member_uidb64": pk, "test_title": test}))
         return render(request=request, template_name=self.template_name, context={
                 "error_message": "Imię albo nazwisko nie jest zgodne ",
@@ -56,6 +58,8 @@ class TestLoginView(CheckOpenedTestMixin, View):
             })
 
 
-class TestDetailView(View):
+class TestDetailView(AuthorizeTeamMemberMixin, View):
+    authorize_team_member_model = TeamMember
+
     def get(self, request, member_uidb64, test_title):
         return render(request=request, template_name="test_app/test_detail.html")
