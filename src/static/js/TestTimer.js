69843629ba1d02a5f944630899d605aaf79720e5
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-04-02 13:37:57 +0200

    added script which sends test form automatically if time is over

--- src/static/js/TestTimer.js
+++ src/static/js/TestTimer.js
@@ -2,7 +2,7 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const performer_time = document.getElementById("performer_time");
-
+const submitButton = document.getElementById("submit_button");
 
 let splittedTime = new String(testDuration).split(":");
 
@@ -10,24 +10,40 @@ hours.innerText = splittedTime[0].toString().padStart(2, "0");
 minutes.innerText = splittedTime[1].toString().padStart(2, "0");
 seconds.innerText = splittedTime[2].toString().padEnd(2, "0");
 
-let timeInSeconds = (Number(splittedTime[0]) * 60 * 60) + (Number(splittedTime[1]) * 60) + Number(splittedTime[2]);
+let timeInSeconds =
+  Number(splittedTime[0]) * 60 * 60 +
+  Number(splittedTime[1]) * 60 +
+  Number(splittedTime[2]);
 
 setInterval(updateTimeRemains, 1000);
 
-function validTimeFormat(timeValue){
-    return timeValue.toString().padStart(2, "0");
+function validTimeFormat(timeValue) {
+  return timeValue.toString().padStart(2, "0");
 };
 
+document.addEventListener("keydown", (event) => {
+    event = event || window.event;
+    if(event.keyCode === 116) event.preventDefault();
+});
 
-function updateTimeRemains(){
-    const hoursRemains = Math.floor(timeInSeconds / 60 / 60);
-    const minutesRemains = Math.floor(timeInSeconds / 60 % 60);
-    const secondsRemains = Math.floor(timeInSeconds % 60);
-    
-    hours.innerText = validTimeFormat(hoursRemains);
-    minutes.innerText = validTimeFormat(minutesRemains);
-    seconds.innerText = validTimeFormat(secondsRemains);
+window.addEventListener("beforeunload", (event) => {
+    event.preventDefault();
+});
 
-    performer_time.value = `${timeInSeconds}`;
-    timeInSeconds--;
-};
+function updateTimeRemains() {
+  if (timeInSeconds < 0){
+    alert("Czas się skończył...");
+    submitButton.click();
+  };
+
+  const hoursRemains = Math.floor(timeInSeconds / 60 / 60);
+  const minutesRemains = Math.floor((timeInSeconds / 60) % 60);
+  const secondsRemains = Math.floor(timeInSeconds % 60);
+
+  hours.innerText = validTimeFormat(hoursRemains);
+  minutes.innerText = validTimeFormat(minutesRemains);
+  seconds.innerText = validTimeFormat(secondsRemains);
+
+  performer_time.value = `${timeInSeconds}`;
+  timeInSeconds--;
+}
