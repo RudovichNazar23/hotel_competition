6e8e2a8d9b91e3a7a270b3c8135bd56bd0dbbf95
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-04-09 18:34:04 +0200

    added token verification for test login page

--- src/test_app/urls.py
+++ src/test_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import TestLoginView, TestDetailView, CompetitionResultDetailView
 
 urlpatterns = [
-    path("test_login_page/<uidb64>", TestLoginView.as_view(), name="test_login"),
+    path("test_login_page/<uidb64>/<token>", TestLoginView.as_view(), name="test_login"),
     path("test_detail/<member_uidb64>/<test_title>", TestDetailView.as_view(), name="test_detail"),
     path("competition_result/<int:pk>", CompetitionResultDetailView.as_view(), name="competition_result")
 ]
