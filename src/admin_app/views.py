3a8c3f375c012abad100abb1ca15f4ce49ff5532
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-04-18 19:52:29 +0200

    added filters for models in admin panel

--- src/admin_app/views.py
+++ src/admin_app/views.py
@@ -2,19 +2,13 @@ from django.http import HttpResponse
 
 from django.views.generic.list import ListView
 from django.views import View
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
-
-from .forms import OpenRegistrationForm
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages.views import SuccessMessageMixin
 
 from registration_app.models import HighSchool, Guardian, TeamMember, SchoolTeam
 
 from service.get_filtered_model_queryset import get_filtered_model_queryset
 from service.mixins.get_model_fields import get_model_fields
-from service.get_model_object import get_model_object
 from service.mixins.csv_serializer_mixin import CsvSerializerMixin
 from service.mixins.csv_writer import CsvWriter
 from service.mixins.get_request_data import RequestObjectDataMixin
@@ -49,53 +43,3 @@ class CreateCsvFileView(LoginRequiredMixin, View, CsvSerializerMixin, RequestObj
 
         return response
 
-
-class NotActivatedSchoolTeamListView(LoginRequiredMixin, ListView):
-    template_name = "admin_app/not_activated_school_team_list.html"
-    context_object_name = "not_activated_school_teams"
-    queryset = get_filtered_model_queryset(model=SchoolTeam, is_active=False)
-    extra_context = {
-        **get_model_fields(model=SchoolTeam, context_name="model_fields")
-    }
-
-
-class SchoolDetailView(LoginRequiredMixin, DetailView):
-    model = HighSchool
-    template_name = "admin_app/school_detail.html"
-    context_object_name = "school"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["school_team"] = get_model_object(model=SchoolTeam, high_school=self.get_object())
-        return context
-
-
-class GuardianDetailView(LoginRequiredMixin, DetailView):
-    model = Guardian
-    template_name = "admin_app/guardian_detail.html"
-    context_object_name = "guardian"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["guardian_school_team"] = get_model_object(model=SchoolTeam, guardian=self.get_object())
-        return context
-
-
-class TeamMemberDetailView(LoginRequiredMixin, DetailView):
-    model = TeamMember
-    template_name = "admin_app/team_member_detail.html"
-    context_object_name = "team_member"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        first_member = get_model_object(model=SchoolTeam, first_member=self.get_object())
-        second_member = get_model_object(model=SchoolTeam, second_member=self.get_object())
-        context["team_member_school_team"] = first_member if first_member is not None else second_member
-        return context
-
-
-class OpenRegistrationView(SuccessMessageMixin, CreateView):
-    template_name = "admin_app/open_registration.html"
-    form_class = OpenRegistrationForm
-    success_url = "open_registration"
-    success_message = "Operacja zakończyła się pomyślnie"
